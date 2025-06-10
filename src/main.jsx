8d20f9d46b785453d66ad3f8cfac82b7cb6aef8d
rafaelgti79 <rafaelgti79@gmail.com>
2025-06-10 12:32:41 -0300

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from "react-dom/client";
+import App from './App.jsx';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import Cadastro from './routes/cadastro.jsx';
+
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: "/sistema-caixa",
+    element: <App />
+  },
+  {
+    path: "/sistema-caixa/cadastro",
+    element: <Cadastro />,
+  },
+]);
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
