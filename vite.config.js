7c38231e4c757d241d844c0c4b96b1487ddc6fda
root <root@srv885417.hstgr.cloud>
2025-06-28 17:55:28 +0000

    Resolvendo conflitos

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/sistema-caixa",
+   
+plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: ['siscafon.top'], // Substitua com seu domínio
+  }
 })
